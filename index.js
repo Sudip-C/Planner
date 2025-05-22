21cbfebe190379784e4197cb813f0f3d1463e7e3
sudip-interleap <sudip.chowdhury001@gmail.com>
2025-05-22 10:33:57 +0530

    app created.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-import { debounce } from "./utils.js";
+import { debounce, throttle } from "./utils.js";
 
 const input = document.getElementById("input-box");
 const button = document.getElementById("add-button");
 const tags = document.querySelectorAll(".items");
 const planList = document.getElementById("plans-container");
 const searchInput = document.getElementById("search-input");
+const clearAll = document.getElementById('clear-all');
+const backToTopBtn = document.getElementById('back-to-top');
+const container = document.getElementById('plans-container')
 
 let selectedTag = null;
 
@@ -32,11 +35,12 @@ const handleSearch = debounce(() => {
 
 searchInput.addEventListener("input", handleSearch);
 
+
 function renderPlans(plans) {
   planList.innerHTML = ""; // clear existing items
 
   plans.forEach((plan, index) => {
-    const li = document.createElement("li");
+    const li = document.createElement("div");
     li.innerHTML = `
       <input type="checkbox"  class="complete-checkbox" ${
         plan.status ? "checked" : ""
@@ -44,13 +48,14 @@ function renderPlans(plans) {
       <span style="text-decoration: ${plan.status ? "line-through" : "none"}">${
       plan.title
     }</span>
-      <span style="margin-left: 10px; font-size: 0.8em; color: gray;">[${
-        plan.tag
-      }]</span>
+      <span class='show-tag' style="margin-left: 10px; font-size: 0.8em; color: gray;">${
+        plan.tag ? plan.tag : "Personal"
+      }</span>
       <button data-index="${index}" class="delete-btn">❌</button>
     `;
     planList.appendChild(li);
   });
+  clearAll.style.display = plans?.length > 0 ? "block": 'none'
 
   const checkboxes = document.querySelectorAll('.complete-checkbox');
   checkboxes.forEach(checkbox => {
@@ -89,4 +94,26 @@ button.addEventListener("click", (e) => {
   input.value = "";
   selectedTag = null;
   tags.forEach((t) => t.classList.remove("selected"));
+});
+
+clearAll.addEventListener('click',()=>{
+  if (confirm('Are you sure you want to delete all tasks?')) {
+  localStorage.removeItem('plans');
+  renderPlans([])
+  }
+})
+
+
+function handleScroll() {
+  if (container.scrollTop > -100) {
+    backToTopBtn.classList.add('show');
+  } else {
+    backToTopBtn.classList.remove('show');
+  }
+}
+
+container.addEventListener('scroll', throttle(handleScroll, 100));
+
+backToTopBtn.addEventListener('click', () => {
+  container.scrollTo({ top: 0, behavior: 'smooth' });
 });
--- utils.js
+++ utils.js
@@ -6,3 +6,14 @@ export function debounce(func, delay) {
     timer = setTimeout(() => func.apply(this, args), delay);
   };
 }
+
+export function throttle(func, limit) {
+  let lastCall = 0;
+  return function (...args) {
+    const now = new Date().getTime();
+    if (now - lastCall >= limit) {
+      lastCall = now;
+      func.apply(this, args);
+    }
+  };
+}
